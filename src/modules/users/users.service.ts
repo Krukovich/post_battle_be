3f2ddc6b80ed0a21a86f1a65b5baf00b9d5fa4ad
Krukovich Alexey <aliaksei.krukovich@itechart-group.com>
2022-07-26 00:10:54 +0300

    refactor: change readme file

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,7 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { prepareURL } from '../../../utils';
-import { IUsers } from "./users.interface";
+import { IUsers } from './users.interface';
 
 @Injectable()
 export class UsersService {
@@ -12,10 +12,7 @@ export class UsersService {
       } = await axios.get(prepareURL(limit));
       return users;
     } catch (error) {
-      throw new HttpException(
-        `${error.message}`,
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      throw new HttpException(`${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
